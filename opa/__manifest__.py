e5ba63b754ab4c1450882bb7f430bc93dbdd8d6f
antonag32 <antonio@vauxoo.com>
2022-03-23 18:24:10 -0600

    [IMP] opa: Added wizard to subscribe attendees to sessions T#55212

--- opa/__manifest__.py
+++ opa/__manifest__.py
@@ -14,9 +14,10 @@
         'data/res_users.xml',
         'views/course_views.xml',
         'views/session_views.xml',
-        'views/ir_ui_menu.xml'
+        'views/ir_ui_menu.xml',
         'views/res_partner_views.xml',
         'data/res_partner_category.xml',
+        'wizard/attendee_wizard_views.xml',
     ],
     'demo': ['demo/course_demo.xml'],
     'application': True,
---
+++ opa/wizard/__init__.py
@@ -0,0 +1,1 @@
+from . import attendee_wizard
